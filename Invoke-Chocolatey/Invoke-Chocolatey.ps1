f1146c3eccfa85e078a1e81078138a80a5a049eb
Aaron Trandem <atrandem@superiormanagedit.com>
2019-12-11 14:28:20 -0600

    Using a smaller help file.

--- Invoke-Chocolatey/Invoke-Chocolatey.ps1
+++ Invoke-Chocolatey/Invoke-Chocolatey.ps1
@@ -1,35 +1,16 @@
-<#
+.<#
 .SYNOPSIS
     Short description
 .DESCRIPTION
     Long description
-.PARAMETER Path
-    Specifies a path to one or more locations.
-.PARAMETER LiteralPath
-    Specifies a path to one or more locations. Unlike Path, the value of LiteralPath is used exactly as it
-    is typed. No characters are interpreted as wildcards. If the path includes escape characters, enclose
-    it in single quotation marks. Single quotation marks tell Windows PowerShell not to interpret any
-    characters as escape sequences.
-.PARAMETER InputObject
-    Specifies the object to be processed.  You can also pipe the objects to this command.
 .EXAMPLE
-    C:\PS>
-    Example of how to use this cmdlet
-.EXAMPLE
-    C:\PS>
-    Another example of how to use this cmdlet
+    Explanation of what the example does
 .INPUTS
-    Inputs to this cmdlet (if any)
+    Inputs (if any)
 .OUTPUTS
-    Output from this cmdlet (if any)
+    Output (if any)
 .NOTES
     General notes
-.COMPONENT
-    The component this cmdlet belongs to
-.ROLE
-    The role this cmdlet belongs to
-.FUNCTIONALITY
-    The functionality that best describes this cmdlet
 #>
 
 
