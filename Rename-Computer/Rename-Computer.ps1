e9f122d7702b64324801342fe91881ca47dbfd28
Aaron Trandem <atrandem@superiormanagedit.com>
2019-12-16 12:34:15 -0600

    adjusted variable name.

--- Rename-Computer/Rename-Computer.ps1
+++ Rename-Computer/Rename-Computer.ps1
@@ -51,22 +51,22 @@ $script:Log = "$script:ScriptPath\$PowershellScriptName.log"
 . .\Get-Chassis.ps1
 
 $ComputerName = $env:COMPUTERNAME
-Invoke-Logging -Message "The current Computer Name is - $ComputerName" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+Invoke-Logging -Message "The current Computer Name is - $ComputerName" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
 
 if ([string]::IsNullOrEmpty($NewName)) {
     #This gets the chassis type, example laptop, NUC, Desktop
     Get-Chassis
     If($isLaptop) { 
         $Chassis = "NB"
-        Invoke-Logging -Message "$env:COMPUTERNAME is a Notebook" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+        Invoke-Logging -Message "$env:COMPUTERNAME is a Notebook" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
     }
     elseif ($isNuc) {
         $Chassis = "NUC"
-        Invoke-Logging -Message "$env:COMPUTERNAME is a Nuc" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+        Invoke-Logging -Message "$env:COMPUTERNAME is a Nuc" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
     }
     else { 
         $Chassis = "DT"
-        Invoke-Logging -Message "$env:COMPUTERNAME is a Desktop" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+        Invoke-Logging -Message "$env:COMPUTERNAME is a Desktop" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
     }
 
     #Grab the last two digits of the year the two digit month, example - 1912 (2019/December(12))
@@ -79,11 +79,11 @@ if ([string]::IsNullOrEmpty($NewName)) {
     #Quick check that machName.txt exists, if it doesn't, we stop the renaming script
     if (!(Test-Path -Path "$script:ScriptPath\RawNames.txt")) {
         Invoke-Logging -Message "The RawNames.txt file is missing. Renaming the machine will stop, manual renaming will need to be done"`
-         -Severity Error -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+         -Severity Error -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
         $host.Exit()
     }
     else {
-        Invoke-Logging -Message "RawNames.txt was found" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+        Invoke-Logging -Message "RawNames.txt was found" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
     }
     #Currently, Kaseya drops a text file that is not how we need it to be to read it correctly
     #This is where we will adjust and create an readable list of machine names
@@ -92,47 +92,47 @@ if ([string]::IsNullOrEmpty($NewName)) {
     #Splits each name into its own line using the splitting by ", "
     $FullName = $RawNames -split (", ")
 
-    Invoke-Logging -Message "Starting foreach loop" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+    Invoke-Logging -Message "Starting foreach loop" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
     foreach ($a in $FullName) {
     #Takes all characters before the first "." and appends it to a file.
     $a.Substring(0,$a.IndexOf(".")) | Out-File -FilePath "$script:ScriptPath\machNames.txt" -Append
     }
-    Invoke-Logging -Message "Finished foreach loop" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+    Invoke-Logging -Message "Finished foreach loop" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
     #Clear $a for reuse
     Clear-Variable -Name "a"
-    Invoke-Logging -Message "Cleared Variable a" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+    Invoke-Logging -Message "Cleared Variable a" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
     #Grab the list of readable names.
     $machName = Get-Content -Path "$ScriptPath\machNames.txt"
     #Compare the name we created to the list of names from machNames.txt
-    Invoke-Logging -Message "Starting do until Loop" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+    Invoke-Logging -Message "Starting do until Loop" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
     $NameCheck = $false
     do {
 
         $NewName = "$Chassis-$DateName-$LastNumber"
 
         if (!($machName -match "$NewName")) {
-            Invoke-Logging -Message "Using Number: $LastNumber Succeded, Using name: $NewName" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+            Invoke-Logging -Message "Using Number: $LastNumber Succeded, Using name: $NewName" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
             $NameCheck = $true
             Break;
         }
         else {
-            Invoke-Logging -Message "Using Number: $LastNumber failed trying next number" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+            Invoke-Logging -Message "Using Number: $LastNumber failed trying next number" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
             $LastNumber++
         }
         
     } until ($NameCheck -eq $true)
 
-    Invoke-Logging -Message "Autobot chose computer name - $NewName" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+    Invoke-Logging -Message "Autobot chose computer name - $NewName" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
 
 }
 else {
-    Invoke-Logging -Message "Manually renaming computer to - $NewName" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+    Invoke-Logging -Message "Manually renaming computer to - $NewName" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
 }
 
 #Rename's computer no reboot / also does one last check to make sure all variables exist
 if (!([string]::IsNullOrEmpty($NewName))) {
     Rename-Computer -NewName "$NewName"
-    Invoke-Logging -Message "Computer has been renamed to $NewName" -Severity Information -Log $Log -FunctionName $CurrentFunction -$PowershellScriptName
+    Invoke-Logging -Message "Computer has been renamed to $NewName" -Severity Information -Log $script:Log -FunctionName $CurrentFunction -$PowershellScriptName
 }
 
 
