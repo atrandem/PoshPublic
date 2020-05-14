5c7fde274b0276ac7ea7fb141b44afb0a7854a15
Aaron Trandem <Aaron@itaaron.com>
2020-05-14 16:22:46 -0500

    Initial commit

---
+++ Invoke-StandardServerBuild/Invoke-StandardServerBuild.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
