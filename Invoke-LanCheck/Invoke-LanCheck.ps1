02541abb4468f92850965bd1e4b0d397e5f01aea
atrandem <ajtrandem@gmail.com>
2021-04-19 15:43:35 -0500

    First commit

---
+++ Invoke-LanCheck/Invoke-LanCheck.ps1
@@ -0,0 +1,53 @@
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $LanPath,
+
+    [Parameter()]
+    [switch]
+    $OutputText = $false,
+
+    [Parameter()]
+    [string]
+    $LogPath
+
+)
+
+
+#Check if lan path is true or not
+if (Test-Path "$LanPath") {
+    #Path exists now output
+    if ($OutputText) {
+        #we output to text file
+        "true" | Out-File -FilePath "$LogPath"
+    }
+    else {
+        return $true
+    }
+}
+else {
+    if ($OutputText) {
+        #we output to text file
+        "false" | Out-File -FilePath "$LogPath"
+    }
+    else {
+        return $false
+    }
+}
