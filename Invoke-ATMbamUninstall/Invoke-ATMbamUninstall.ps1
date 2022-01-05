bb7b35406d0acefd106499750f3eb674f8037096
atrandem <ajtrandem@gmail.com>
2022-01-05 16:32:06 -0600

    Updated script lots of adjustments

--- Invoke-ATMbamUninstall/Invoke-ATMbamUninstall.ps1
+++ Invoke-ATMbamUninstall/Invoke-ATMbamUninstall.ps1
@@ -11,104 +11,136 @@
 .OUTPUTS
     Output (if any)
 .NOTES
-    General notes
+    Credit goes to this poster on Reddit, I edited to fit my needs.
+	https://www.reddit.com/r/msp/comments/hsglwk/av_removal_script/
 #>
+
+[CmdletBinding()]
+param (
+	[Parameter()]
+	[string]
+	$Log = "C:Scripts\Logs\MbamUninstallLog.txt"
+)
+
+"$env:COMPUTERNAME : Beginning search\uninstall of Malwarebytes"
 
 $MbAMCheck = (Resolve-Path -Path C:\Prog*\Malw*).Path
 $MbAMCheck += (Resolve-Path -Path C:\Prog*\Malw*\Ant*).Path
 
+
 if (!([string]::IsNullOrEmpty($MbAMCheck)))
 {
-	Write-Host "Found Malwarebytes software..." -ForegroundColor Green
-	Write-Host "Checking for Malwarebytes Uninstaller..." -ForegroundColor Yellow
-	if ((Test-Path -Path C:\Temp\mbstcmd.exe) -eq $true)
-	{
-		Write-Host "Found Command line Malwarebytes Uninstaller." -ForegroundColor Green
-		Write-Host "Running Command line Malwarebytes Uninstaller Silently..." -ForegroundColor Yellow
-		Start-Process -FilePath C:\Temp\mbstcmd.exe -ArgumentList "/y", "/cleanup", "/noreboot" -Wait
-		Write-Host "Removed Malwarebytes." -ForegroundColor Green
-		Write-Host "Checking for any other installations..." -ForegroundColor Yellow
-	}
-	else
-	{
-		Write-Host "Uninstaller not found! Manually checking for other installations..." -ForegroundColor Yellow
-	}
-	
+
 	## Checking for all installations of Malwarebytes. Installations have changed paths over version changes. Removing if found.
 	if ((Test-Path -Path "C:\Program Files\Malwarebytes' Anti-Malware\unins000.exe") -eq $true)
 	{
-		Write-Host "Found Malwarebytes..." -ForegroundColor Green
-		Write-Host "Removing Malwarebytes..." -ForegroundColor Yellow
+		"Found MBAM in: C:\Program Files\Malwarebytes' Anti-Malware\unins000.exe" | Out-File $Log -Append
 		Start-Process -FilePath "C:\Program Files\Malwarebytes' Anti-Malware\unins000.exe" -ArgumentList "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART" -Wait
-		Write-Host "Removed Malwarebytes." -ForegroundColor Green
+		#Checking that its been uninstalled. Path may still exist and 2 files would be hanging around.
+		if (Test-Path -Path "C:\Program Files\Malwarebytes' Anti-Malware\mbam.exe") {
+			"MBAM was FAILED uninstalled, please check manually" | Out-File $Log -Append
+		}
+		else {
+			"MBAM has SUCCESSFULLY uninstalled" | Out-File $Log -Append
+		}
+
 	}
 	
 	if ((Test-Path -Path "C:\Program Files (x86)\Malwarebytes' Anti-Malware\unins000.exe") -eq $true)
 	{
-		Write-Host "Found Malwarebytes..." -ForegroundColor Green
-		Write-Host "Removing Malwarebytes..." -ForegroundColor Yellow
+		"Found MBAM in: C:\Program Files (x86)\Malwarebytes' Anti-Malware\unins000.exe" | Out-File $Log -Append
 		Start-Process -FilePath "C:\Program Files (x86)\Malwarebytes' Anti-Malware\unins000.exe" -ArgumentList "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART" -Wait
-		Write-Host "Removed Malwarebytes." -ForegroundColor Green
+		#Checking that its been uninstalled. Path may still exist and 2 files would be hanging around.
+		if (Test-Path -Path "C:\Program Files (x86)\Malwarebytes' Anti-Malware\mbam.exe") {
+			"MBAM was FAILED uninstalled, please check manually" | Out-File $Log -Append
+		}
+		else {
+			"MBAM has SUCCESSFULLY uninstalled" | Out-File $Log -Append
+		}
 	}
 	
 	if ((Test-Path -Path "C:\Program Files\Malwarebytes Anti-Malware\unins000.exe") -eq $true)
 	{
-		Write-Host "Found Malwarebytes..." -ForegroundColor Green
-		Write-Host "Removing Malwarebytes..." -ForegroundColor Yellow
+		"Found MBAM in: C:\Program Files\Malwarebytes Anti-Malware\unins000.exe" | Out-File $Log -Append
 		Start-Process -FilePath "C:\Program Files\Malwarebytes Anti-Malware\unins000.exe" -ArgumentList "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART" -Wait
-		Write-Host "Removed Malwarebytes." -ForegroundColor Green
+		#Checking that its been uninstalled. Path may still exist and 2 files would be hanging around.
+		if (Test-Path -Path "C:\Program Files\Malwarebytes Anti-Malware\mbam.exe") {
+			"MBAM was FAILED uninstalled, please check manually" | Out-File $Log -Append
+		}
+		else {
+			"MBAM has SUCCESSFULLY uninstalled" | Out-File $Log -Append
+		}
 	}
 	
 	if ((Test-Path -Path "C:\Program Files (x86)\Malwarebytes Anti-Malware\unins000.exe") -eq $true)
 	{
-		Write-Host "Found Malwarebytes..." -ForegroundColor Green
-		Write-Host "Removing Malwarebytes..." -ForegroundColor Yellow
+		"Found MBAM in: C:\Program Files (x86)\Malwarebytes Anti-Malware\unins000.exe" | Out-File $Log -Append
 		Start-Process -FilePath "C:\Program Files (x86)\Malwarebytes Anti-Malware\unins000.exe" -ArgumentList "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART" -Wait
-		Write-Host "Removed Malwarebytes." -ForegroundColor Green
+		#Checking that its been uninstalled. Path may still exist and 2 files would be hanging around.
+		if (Test-Path -Path "C:\Program Files (x86)\Malwarebytes Anti-Malware\mbam.exe") {
+			"MBAM was FAILED uninstalled, please check manually" | Out-File $Log -Append
+		}
+		else {
+			"MBAM has SUCCESSFULLY uninstalled" | Out-File $Log -Append
+		}
 	}
 	
 	if ((Test-Path -Path "C:\Program Files\Malwarebytes\Anti-Malware\unins000.exe") -eq $true)
 	{
-		Write-Host "Found Malwarebytes..." -ForegroundColor Green
-		Write-Host "Removing Malwarebytes..." -ForegroundColor Yellow
+		"Found MBAM in: C:\Program Files\Malwarebytes\Anti-Malware\unins000.exe" | Out-File $Log -Append
 		Start-Process -FilePath "C:\Program Files\Malwarebytes\Anti-Malware\unins000.exe" -ArgumentList "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART" -Wait
-		Write-Host "Removed Malwarebytes." -ForegroundColor Green
+		#Checking that its been uninstalled. Path may still exist and 2 files would be hanging around.
+		if (Test-Path -Path "C:\Program Files\Malwarebytes\Anti-Malware\mbam.exe") {
+			"MBAM was FAILED uninstalled, please check manually" | Out-File $Log -Append
+		}
+		else {
+			"MBAM has SUCCESSFULLY uninstalled" | Out-File $Log -Append
+		}		
 	}
 	
 	if ((Test-Path -Path "C:\Program Files (x86)\Malwarebytes\Anti-Malware\unins000.exe") -eq $true)
 	{
-		Write-Host "Found Malwarebytes..." -ForegroundColor Green
-		Write-Host "Removing Malwarebytes..." -ForegroundColor Yellow
+		"Found MBAM in: C:\Program Files\Malwarebytes' Anti-Malware\unins000.exe" | Out-File $Log -Append
 		Start-Process -FilePath "C:\Program Files (x86)\Malwarebytes\Anti-Malware\unins000.exe" -ArgumentList "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART" -Wait
-		Write-Host "Removed Malwarebytes." -ForegroundColor Green
+		#Checking that its been uninstalled. Path may still exist and 2 files would be hanging around.
+		if (Test-Path -Path "C:\Program Files (x86)\Malwarebytes\Anti-Malware\mbam.exe") {
+			"MBAM was FAILED uninstalled, please check manually" | Out-File $Log -Append
+		}
+		else {
+			"MBAM has SUCCESSFULLY uninstalled" | Out-File $Log -Append
+		}		
 	}
 	
 	if ((Test-Path -Path "C:\Program Files (x86)\Malwarebytes\Anti-Malware\mbuns.exe") -eq $true)
 	{
-		Write-Host "Found Malwarebytes..." -ForegroundColor Green
-		Write-Host "Removing Malwarebytes..." -ForegroundColor Yellow
+		"Found MBAM in: C:\Program Files\Malwarebytes' Anti-Malware\unins000.exe" | Out-File $Log -Append
 		Start-Process -FilePath "C:\Program Files (x86)\Malwarebytes\Anti-Malware\mbuns.exe" -ArgumentList "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART" -Wait
-		Write-Host "Removed Malwarebytes." -ForegroundColor Green
+		#Checking that its been uninstalled. Path may still exist and 2 files would be hanging around.
+		if (Test-Path -Path "C:\Program Files (x86)\Malwarebytes\Anti-Malware\mbam.exe") {
+			"MBAM was FAILED uninstalled, please check manually" | Out-File $Log -Append
+		}
+		else {
+			"MBAM has SUCCESSFULLY uninstalled" | Out-File $Log -Append
+		}		
 	}
 	
 	if ((Test-Path -Path "C:\Program Files\Malwarebytes\Anti-Malware\mbuns.exe") -eq $true)
 	{
-		Write-Host "Found Malwarebytes..." -ForegroundColor Green
-		Write-Host "Removing Malwarebytes..." -ForegroundColor Yellow
+		"Found MBAM in: C:\Program Files\Malwarebytes' Anti-Malware\unins000.exe" | Out-File $Log -Append
 		Start-Process -FilePath "C:\Program Files\Malwarebytes\Anti-Malware\mbuns.exe" -ArgumentList "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART" -Wait
-		Write-Host "Removed Malwarebytes." -ForegroundColor Green
+		#Checking that its been uninstalled. Path may still exist and 2 files would be hanging around.
+		if (Test-Path -Path "C:\Program Files\Malwarebytes\Anti-Malware\mbam.exe") {
+			"MBAM was FAILED uninstalled, please check manually" | Out-File $Log -Append
+		}
+		else {
+			"MBAM has SUCCESSFULLY uninstalled" | Out-File $Log -Append
+		}		
 	}
-	
-	if ((Get-Package -Name Malwarebytes*) -eq $true)
-	{
-		Write-Host "Found Malwarebytes..." -ForegroundColor Green
-		Write-Host "Removing Malwarebytes..." -ForegroundColor Yellow
-		Get-Package -Name Malwarebytes* | Uninstall-Package -AllVersions -Force
-	}
-	Write-Host "Malwarebytes removal completed." -ForegroundColor Green
 }
 else
 {
-	Write-Host "No Malwarebytes software found." -ForegroundColor Yellow
-	Write-Host "Continuing..." -ForegroundColor Green
+	"No Malwarebytes software found." | Out-File $Log -Append
+
 }
+
+
