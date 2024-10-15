8e348b6aa316f2c98e864cc05c4ab601a15a2165
Aaron Trandem <ajtrandem@gmail.com>
2024-10-15 09:59:05 -0500

    Added install and uninstall parameters and logic to execute both.

--- Invoke-DattoFileProtect/Invoke-DattoFileProtect.ps1
+++ Invoke-DattoFileProtect/Invoke-DattoFileProtect.ps1
@@ -1,34 +1,47 @@
 <#
 .SYNOPSIS
-    Short description
+    Either install or unintall Datto File Protect
 .DESCRIPTION
     Long description
 .EXAMPLE
-    Explanation of what the example does
+    .\Invoke-DattoFileProtect.ps1 -Install -Path C:\Temp
+    .\Invoke-DattoFileProtect.ps1 -Uninstall -Path C:\Temp
+    .\Invoke-DattoFileProtect.ps1 -Install
+    .\Invoke-DattoFileProtect.ps1 -Uninstall
 .INPUTS
     Inputs (if any)
 .OUTPUTS
     Output (if any)
 .NOTES
-    General notes
+    Created By Aaron Trandem
 #>
 
-function Invoke-DattoFileProtect {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory=$false)]
-        [string[]]
-        $Path
-    )
-    
-    if ([string]::IsNullOrWhiteSpace($Path)) {
-        $Path = $MyInvocation.PSScriptRoot
-    }
+[CmdletBinding(DefaultParameterSetName='None')]
+param (
+    [Parameter(Mandatory=$true, ParameterSetName='Install')]
+    [Parameter(Mandatory=$true, ParameterSetName='Uninstall')]
+    [string[]]
+    $Path,
 
-    Invoke-WebRequest -Uri "https://us.fileprotection.datto.com/update/aeb/DattoFileProtectionSetup_v8.1.0.59.exe" -OutFile "$Path\DattoFileProtect.exe" -UseBasicParsing
-    
+    [Parameter(Mandatory=$true, ParameterSetName='Install')]
+    [string[]]
+    $Install,
+
+    [Parameter(Mandatory=$true, ParameterSetName='Uninstall')]
+    [string[]]
+    $Uninstall
+)
+
+Invoke-WebRequest -Uri "https://us.fileprotection.datto.com/update/aeb/DattoFileProtectionSetup_v8.1.0.59.exe" -OutFile "$Path\DattoFileProtect.exe" -UseBasicParsing
+
+if ($PSCmdlet.ParameterSetName -eq 'Install') {
+    Write-Output "Installing with path: $Path"
     Start-Process -FilePath "$Path\DattoFileProtect.exe" -ArgumentList "/install /quiet /norestart" -Wait
-
 }
 
-Invoke-DattoFileProtect
+if ($PSCmdlet.ParameterSetName -eq 'Uninstall') {
+    Write-Output "Uninstalling with path: $Path"
+    Start-Process -FilePath "$Path\DattoFileProtect.exe" -ArgumentList "/uninstall /quiet /norestart" -Wait
+}
+
+#End
